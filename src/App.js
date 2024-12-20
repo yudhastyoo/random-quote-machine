24d67916f372b635bab7e67fa44dcc594a0b84fd
Yudha Papua Setyo Atmaji <yudhapapua15@gmail.com>
2024-12-20 18:01:20 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import { quotes } from "./quotes";
+
+function App() {
+  const [quote, setQuote] = useState({});
+  const [fade, setFade] = useState(false);
+
+  const getRandomQuote = () => {
+    setFade(true);
+    setTimeout(() => {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      setQuote(quotes[randomIndex]);
+      setFade(false);
+    }, 500);
+  };
+
+  useEffect(() => {
+    getRandomQuote();
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="quote-box">
+        <div className="quote-text">
+          <span id="text" style={{ opacity: fade ? 0 : 1 }}>
+            "{quote.quote}"
+          </span>
+        </div>
+        <div class="quote-author">
+          <span id="author">- {quote.author}</span>
+        </div>
+        <div class="buttons">
+          <a
+            class="button"
+            id="tweet-quote"
+            href={`https://twitter.com/intent/tweet?text=${quote.quote} - ${quote.author}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tweet Quote
+          </a>
+          <button class="button" id="new-quote" onClick={getRandomQuote}>
+            New Quote
+          </button>
+        </div>
+      </div>
+      <div className="watermark">
+        By <span>Yudha Papua Setyo Atmaji</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
